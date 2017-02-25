f3f3afb004df339fec06f8d11e462e902a14af38
Darya Tikhomirova <tihodrik@gmail.com>
2017-02-25 23:53:54 +0300

    Comments added

--- sql_task.sql
+++ sql_task.sql
@@ -46,23 +46,27 @@ insert into students (first_name, middle_name, last_name, course, gender, city_i
     ('Zhen','Chi','Bao','2','m','4');
     
 
-
+#Сколько всего студентов приехало к нам учиться
 select count(student_id) from students;
 
+#Сколько студенток-девочек приехало учиться и на каких они курсах
 select count(student_id) from students where gender='f';
 select course from students where gender='f' group by course;
 
+#Сколько студентов приехало учиться из Германии
 select count(*) from students, cities where students.city_id = cities.city_id and cities.country = 'Germany';
 
+#Сколько студентов младше четвертого курса у нас обучаются (не включая сам 4 курс)
 select count(student_id) from students where course<'4';
 
+#Необходимо перевести Анну со 2 на 3 курс, а Питера за неуспеваемость на второй курс
 update students set course='3' where first_name='Anna';
 update students set course='2' where first_name='Peter';
 
-
+#Необходимо удалить записи обо всех студентках-девушках из Германии, т.к. им не дали разрешение на обучение у нас
 delete from students where gender='f' and students.city_id in (select city_id from cities where country='Germany');
 
+#Всем студентам необходимо добавить данные об отметке об успешном освоении нашего курса, по-умолчанию у всех курс не освоен, кроме студентов 4-го курса из Германии
 alter table students add column finished bool default '0' after gender;
-
 update students set finished='1' where course='4' and city_id in (select city_id from cities where country = 'Germany');
 
